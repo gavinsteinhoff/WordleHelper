44ca7105fe614444f9836cee6b5c564ead1d3546
Gavin Steinhoff <GavinSteinhoff@gmail.com>
2022-02-14 15:21:05 -0600

    Moved Logic. Added Confirmed Letters

---
+++ WordleHelper/WordleHelperClasses.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WordleHelper
+{
+    public class DictionaryData
+    {
+        public List<string> Words { get; set; } = new List<string>();
+    }
+    public class WordleHelperInput
+    {
+        [Required]
+        [StringLength(maximumLength: 5, MinimumLength = 5, ErrorMessage = "Skeleton Must Be 5 Characters")]
+        public string Skeleton { get; set; } = "";
+        public string BlockedLetters { get; set; } = "";
+        public string ConfirmedLetters { get; set; } = "";
+    }
+}
---
+++ WordleHelper/WordleHelperLogic.cs
@@ -0,0 +1,65 @@
+﻿using System.Net.Http.Json;
+using System.Text.RegularExpressions;
+
+namespace WordleHelper;
+
+public interface IWordleHelperLogic
+{
+    Task GetWords();
+    IEnumerable<string> GetSolutions(WordleHelperInput model);
+}
+
+public class WordleHelperLogic : IWordleHelperLogic
+{
+    private readonly HttpClient _http;
+    private string _words = "";
+
+    public WordleHelperLogic(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task GetWords()
+    {
+        var response = await _http.GetFromJsonAsync<DictionaryData>("dictionaries/wordle.json");
+        if (response is not null)
+        {
+            var rnd = new Random();
+            var randomized = response.Words.OrderBy(item => rnd.Next());
+            _words = string.Join(",", randomized);
+        }
+    }
+
+    public IEnumerable<string> GetSolutions(WordleHelperInput model)
+    {
+        var blockedRegex = model.BlockedLetters.Length > 0 ? $"[^{model.BlockedLetters}]" : ".";
+        var regexBuilder = @"\b";
+        foreach (var letter in model.Skeleton)
+        {
+            if (letter == '_')
+                regexBuilder += blockedRegex;
+            else
+                regexBuilder += (letter.ToString());
+        }
+        regexBuilder += @"\b";
+
+        Regex regex = new Regex(regexBuilder, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        var potentialMatches = regex.Matches(_words).Select(m => m.Value).ToList();
+        var matches = potentialMatches;
+
+        if (!string.IsNullOrEmpty(model.ConfirmedLetters))
+        {
+            matches = new List<string>();
+            potentialMatches.ForEach(match =>
+            {
+                var passed = true;
+                model.ConfirmedLetters.ToCharArray().ToList().ForEach(letter =>
+                {
+                    if (!match.Contains(letter)) passed = false;
+                });
+                if (passed) matches.Add(match);
+            });
+        }
+        return matches.Take(100);
+    }
+}
